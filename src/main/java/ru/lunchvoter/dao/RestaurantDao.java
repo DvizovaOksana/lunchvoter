20c280f5445142f4d6bf9d23dd75d64d63454413
Dvizova Oksana <vieviersa@gmail.com>
2020-08-05 17:32:03 +0300

    Add Restaurant dao, service

--- src/main/java/ru/lunchvoter/dao/RestaurantDao.java
+++ src/main/java/ru/lunchvoter/dao/RestaurantDao.java
@@ -1,9 +1,33 @@
 package ru.lunchvoter.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.lunchvoter.model.Restaurant;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Transactional(readOnly = true)
 public interface RestaurantDao extends JpaRepository<Restaurant, Integer> {
+    @Override
+    @Transactional
+    Restaurant save(Restaurant restaurant);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Restaurant r WHERE r.id=:id")
+    int delete(@Param("id") int id);
+
+    @Override
+    Optional<Restaurant> findById(Integer id);
+
+    @Query("SELECT r FROM Restaurant r LEFT JOIN FETCH r.meals m WHERE r.id=:id AND m.date=:date ORDER BY r.name")
+    Optional<Restaurant> getWithDishesForDate(@Param("id") int id, @Param("date") LocalDate date);
+
+    @Query("SELECT DISTINCT r FROM Restaurant r LEFT JOIN FETCH r.meals m WHERE m.date=:date ORDER BY r.name")
+    List<Restaurant> getAllWithDishesForDate(LocalDate date);
 }
--- src/main/java/ru/lunchvoter/service/RestaurantService.java
+++ src/main/java/ru/lunchvoter/service/RestaurantService.java
@@ -1,16 +1,55 @@
 package ru.lunchvoter.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import ru.lunchvoter.dao.MealDao;
 import ru.lunchvoter.dao.RestaurantDao;
+import ru.lunchvoter.model.Restaurant;
+import ru.lunchvoter.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static ru.lunchvoter.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class RestaurantService {
-    private final RestaurantDao restaurantDao;
-    private final MealDao mealDao;
+    private final RestaurantDao dao;
 
-    public RestaurantService(RestaurantDao restaurantDao, MealDao mealDao) {
-        this.restaurantDao = restaurantDao;
-        this.mealDao = mealDao;
+    public RestaurantService(RestaurantDao dao, MealDao mealDao) {
+        this.dao = dao;
     }
+
+    public Restaurant create(Restaurant restaurant){
+        Assert.notNull(restaurant, "restaurant must not be null");
+        return dao.save(restaurant);
+    }
+
+    public Restaurant get(int restaurantId) throws NotFoundException{
+        return checkNotFoundWithId(dao.findById(restaurantId)
+                .orElse(null), restaurantId);
+    }
+
+    public List<Restaurant> getAll(){
+        return dao.findAll();
+    }
+
+    public Restaurant getWithDishesForDate(int id, LocalDate date){
+        return checkNotFoundWithId(dao.getWithDishesForDate(id, date)
+                .orElse(null), id);
+    }
+
+    public List<Restaurant> getAllWithDishesForDate(LocalDate date){
+        return dao.getAllWithDishesForDate(date);
+    }
+
+    public void update(Restaurant restaurant){
+        Assert.notNull(restaurant, "restaurant must not be null");
+        checkNotFoundWithId(dao.save(restaurant), restaurant.getId());
+    }
+
+    public void delete(int id){
+        checkNotFoundWithId(dao.delete(id) != 0, id);
+    }
+
 }
