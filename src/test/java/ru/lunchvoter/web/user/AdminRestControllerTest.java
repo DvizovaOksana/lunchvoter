799cd6dff89c286d48c968947f460b61cfb881e6
Dvizova Oksana <vieviersa@gmail.com>
2020-08-13 14:47:38 +0300

    Add AdminRestControllerTest tests

---
+++ src/test/java/ru/lunchvoter/web/user/AdminRestControllerTest.java
@@ -0,0 +1,91 @@
+package ru.lunchvoter.web.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.lunchvoter.model.User;
+import ru.lunchvoter.service.UserService;
+import ru.lunchvoter.util.exception.NotFoundException;
+import ru.lunchvoter.util.json.JsonUtil;
+import ru.lunchvoter.web.AbstractControllerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.lunchvoter.TestUtil.readFromJson;
+import static ru.lunchvoter.UserTestData.*;
+import static ru.lunchvoter.UserTestData.ADMIN_ID;
+
+class AdminRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(USER_MATCHER.contentJson(USER, ADMIN));
+    }
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + ADMIN_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(USER_MATCHER.contentJson(ADMIN));
+    }
+
+    @Test
+    void createWithLocation() throws Exception {
+        User newUser = getNew();
+
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newUser)))
+                .andExpect(status().isCreated());
+
+        User created = readFromJson(action, User.class);
+        int newId = created.getId();
+        newUser.setId(newId);
+
+        USER_MATCHER.assertMatch(created, newUser);
+        USER_MATCHER.assertMatch(userService.get(newId), newUser);
+    }
+
+    @Test
+    void delete() throws Exception {
+        ResultActions action = perform(MockMvcRequestBuilders.delete(REST_URL + USER_ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+        assertThrows(NotFoundException.class, () -> userService.get(USER_ID));
+    }
+
+    @Test
+    void update() throws Exception {
+        User updatedUser = getUpdated();
+
+        ResultActions action = perform(MockMvcRequestBuilders.put(REST_URL + USER_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updatedUser)))
+                .andExpect(status().isNoContent());
+
+        USER_MATCHER.assertMatch(userService.get(USER_ID), updatedUser);
+    }
+
+    @Test
+    void getByMail() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "by?email=" + ADMIN.getEmail()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(USER_MATCHER.contentJson(ADMIN));
+    }
+}
